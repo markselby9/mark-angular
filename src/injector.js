27aa0c09be7a4c767c04829cd5b04b7c2e1d3059
markselby <markselbyfcy@gmail.com>
2015-12-25 21:05:01 +0800

    Instantiating Objects with DI

--- src/injector.js
+++ src/injector.js
@@ -4,10 +4,14 @@
 
 'use strict';
 var _ = require('lodash');
+var FN_ARGS = /^function\s*[^\(]*\(\s*([^)]*)\)/m;  //reg expression
+var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/;
+var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg;    //? means lazy quantifier, g means multiple matches
 
-function createInjector(modulesToLoad){
+function createInjector(modulesToLoad, strictDI){   //strict dependency injection mode
     var cache = {};
     var loadedModules = {};
+    strictDI = (strictDI === true); //must be true
 
     var $provide = {
         constant: function(key, value){
@@ -20,18 +24,47 @@ function createInjector(modulesToLoad){
     };
 
     function invoke(fn, self, locals){  //self is the given context
-        var args = _.map(fn.$inject, function(token){
+        var args = _.map(annotate(fn), function(token){
             if (_.isString(token)){
                 return locals && locals.hasOwnProperty(token)? locals[token]: cache[token];
             }else{
                 throw 'incorrect injectiong token! '+token+ ' not a string!';
             }
         });
+        if (_.isArray(fn)){
+            fn = _.last(fn);
+        }
         return fn.apply(self, args);
     }
 
-    function annotate(fn){
-        return fn.$inject;
+    function annotate(fn){  //annotate an injected function
+        if (_.isArray(fn)){
+            return fn.slice(0, fn.length-1);    //remove the last item
+        }else if (fn.$inject){
+            return fn.$inject;
+        }
+        else if (!fn.length){
+            return [];
+        }
+        else{ //if neither $inject nor array wrapping defined
+            if (strictDI){
+                throw 'fn is not using explicit annotation and cannot be invoked ' +
+                'in strict mode';
+            }
+            var sourceStrippedComments = fn.toString().replace(STRIP_COMMENTS, '');//strip the comments in the function tostring
+            var argDeclaration = sourceStrippedComments.match(FN_ARGS);
+            var arg_arr = argDeclaration[1].split(',');
+            return _.map(arg_arr, function(argName){
+                return argName.match(FN_ARG)[2];
+            });
+        }
+    }
+
+    function instantiate(Type, locals){
+        var UnwrappedType = _.isArray(Type)? _.last(Type):Type;
+        var instance = Object.create(UnwrappedType.prototype);
+        invoke(Type, instance, locals);
+        return instance;
     }
 
     _.forEach(modulesToLoad, function loadModule(moduleName){
@@ -54,7 +87,8 @@ function createInjector(modulesToLoad){
             return cache[key];
         },
         invoke: invoke,
-        annotate: annotate
+        annotate: annotate,
+        instantiate: instantiate
     };
 }
 
