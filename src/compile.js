c2ce44c47b9fa6473b206367feb97597cda9e00d
markselby <markselbyfcy@gmail.com>
2016-02-08 14:53:06 +0800

    finish basic DOM compilation and directives

--- src/compile.js
+++ src/compile.js
@@ -25,9 +25,12 @@ function $CompileProvider($provide){
                 $provide.factory(name+'Directive', ['$injector', function($injector){
                     //looks up the directive factories from internal registry
                     var factories = hasDirectives[name];
-                    return _.map(factories, function(factory){
+                    return _.map(factories, function(factory, i){
                         var directive = $injector.invoke(factory);
                         directive.restrict = directive.restrict || 'EA';    //'EA' by default
+                        directive.priority = directive.priority || 0;
+                        directive.name = directive.name || name;
+                        directive.index = i;
                         return directive;
                     });
                 }]);
@@ -38,8 +41,6 @@ function $CompileProvider($provide){
                 this.directive(name, directiveFactory);
             }, this);
         }
-
-
     };
 
     this.$get = ['$injector', function($injector){
@@ -51,8 +52,8 @@ function $CompileProvider($provide){
             //iterate over the given jQuery object
             _.forEach($compileNodes, function(node){
                 var directives = collectDirectives(node);
-                applyDirectivesToNode(directives, node);
-                if (node.childNodes && node.childNodes.length){
+                var terminal = applyDirectivesToNode(directives, node);
+                if (!terminal && node.childNodes && node.childNodes.length){
                     compileNodes(node.childNodes);
                 }
             });
@@ -60,11 +61,47 @@ function $CompileProvider($provide){
 
         function applyDirectivesToNode(directives, compileNode){
             var $compileNode = $(compileNode);//jquery wrap
+            var terminalPriority = -Number.MAX_VALUE;
+            var terminal = false;
             _.forEach(directives, function(directive){
+                if (directive.$$start){
+                    //replace the nodes passed to compile with the start and end nodes and any siblings in between
+                    $compileNode = groupScan(compileNode, directive.$$start, directive.$$end);
+                }
+                if (directive.priority < terminalPriority){
+                    return false;
+                }
                 if (directive.compile){
                     directive.compile($compileNode);
                 }
+                if (directive.terminal){
+                    terminal = true;
+                    terminalPriority = directive.priority;
+                }
             });
+            return terminal;
+        }
+
+        function groupScan(node, startAttr, endAttr){
+            var nodes = [];
+            if (startAttr && node && node.hasAttribute(startAttr)){
+                //the function begins collecting the group
+                var depth = 0;
+                do{
+                    if (node.nodeType === Node.ELEMENT_NODE){
+                        if (node.hasAttribute(startAttr)){
+                            depth++;
+                        }else if (node.hasAttribute(endAttr)){
+                            depth--;
+                        }
+                    }
+                    nodes.push(node);
+                    node = node.nextSibling;
+                } while (depth>0);
+            }else{
+                nodes.push(node);
+            }
+            return $(nodes);
         }
 
         // find what directives apply to given DOM node
@@ -78,11 +115,24 @@ function $CompileProvider($provide){
 
                 // matching by attributes
                 _.forEach(node.attributes, function(attribute){
-                    var normalizedAttrName = directiveNormalize(attribute.name.toLowerCase());
-                    if (/^ngAttr[A-Z]/.test(normalizedAttrName)){
-                        normalizedAttrName = normalizedAttrName[6].toLowerCase() + normalizedAttrName.substring(7);
+                    var attributeStartName, attributeEndName;
+                    var name = attribute.name;
+                    var normalizedAttrName = directiveNormalize(name.toLowerCase());
+                    if (/^ngAttr[A-Z]/.test(normalizedAttrName)){   //remove ng-attr- prefix
+                        //normalizedAttrName = normalizedAttrName[6].toLowerCase() + normalizedAttrName.substring(7);
+                        name = _.kebabCase(normalizedAttrName[6].toLowerCase()+normalizedAttrName.substring(7));
                     }
-                    addDirective(directives, normalizedAttrName, 'A');
+
+                    var directiveNName = normalizedAttrName.replace(/(Start|End)$/, ''); //deal with Start or End suffixes
+                    if (directiveIsMultiElement(directiveNName)){
+                        if (/Start$/.test(normalizedAttrName)){
+                            attributeStartName = name;
+                            attributeEndName = name.substring(0, name.length-5)+'end';
+                            name = name.substring(0, name.length-6);
+                        }
+                    }
+                    normalizedAttrName = directiveNormalize(name.toLowerCase());
+                    addDirective(directives, normalizedAttrName, 'A', attributeStartName, attributeEndName);
                 });
 
                 //matching by class
@@ -98,7 +148,33 @@ function $CompileProvider($provide){
                 }
             }
 
+            directives.sort(byPriority);
             return directives;
+        }
+
+        function directiveIsMultiElement(name){
+            if (hasDirectives.hasOwnProperty(name)){
+                // if exists directives registered with name
+                var directives = $injector.get(name+'Directive');
+                return _.any(directives, {multiElement:true});
+                // if multiElement flag set to true
+            }
+            return false;
+        }
+
+        // sort directives by priority
+        function byPriority(a,b){
+            var diff = b.priority - a.priority;
+            if (diff!==0){
+                return diff;
+            }else{
+                var diffname = a.name < b.name?-1:1;
+                if (diffname !== 0){
+                    return diffname;
+                } else{
+                    return a.index - b.index;
+                }
+            }
         }
 
         // raw DOM node or jQuery wrapped one
@@ -107,13 +183,22 @@ function $CompileProvider($provide){
         }
 
         // checks if the local hasDirectives array has directives with that name.
-        function addDirective(directives, name, mode){
+        function addDirective(directives, name, mode, attrStartName, attrEndName){
             if (hasDirectives.hasOwnProperty(name)){
                 var foundDirectives = $injector.get(name+'Directive');
                 var applicableDirectives = _.filter(foundDirectives, function(dir){
                     return dir.restrict.indexOf(mode) !== -1;   //filter matching directives of current mode
                 });
-                directives.push.apply(directives, applicableDirectives);
+                _.forEach(applicableDirectives, function(directive){
+                    if (attrStartName){
+                        //attach that to the directive object with the special keys $$start and $$end
+                        directive = _.create(directive, {
+                            $$start: attrStartName,
+                            $$end: attrEndName
+                        });
+                    }
+                    directives.push(directive);
+                });
                 //push.apply concatenate the array to directives
             }
         }
